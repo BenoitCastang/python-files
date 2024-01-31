c88e7b925f0d0d45facd61535fdd95ff3f4c5d5d
Benoit Castang <castang.benoit@gmail.com>
2024-01-31 07:40:35 +0100

    Added notes, scripts refactored partially

--- vowelTest.py
+++
@@ -1,8 +0,0 @@
-string = 'Le loup a faim'
-for letter in string :
-    if letter in 'AEIOUYaeiouy' :
-        print(letter, 'est une voyelle')
-    elif letter in ' ' :
-        print('*')
-    else : 
-        print(letter, 'est une consonne')
